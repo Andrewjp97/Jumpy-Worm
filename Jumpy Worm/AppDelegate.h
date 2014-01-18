925fae0e5dfa5a5f1509c000afb38ef286528603
Andrew Paterson <Andrewjp97@me.com>
2014-01-18 09:53:19 -0500

    Initial Commit

---
+++ Jumpy Worm/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Jumpy Worm
+//
+//  Created by Andrew Paterson on 1/18/14.
+//  Copyright (c) 2014 Andrew Paterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jumpy Worm/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Jumpy Worm
+//
+
+//  Copyright (c) 2014 Andrew Paterson. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
